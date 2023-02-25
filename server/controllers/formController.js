5f61c7379ccd3a54180c199bd4963782fa28515f
MuhammadFahadAkbar <70443460+MuhammadFahadAkbar@users.noreply.github.com>
2023-02-25 16:31:34 +0500

    Update formController.js

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -52,29 +52,23 @@ const editForm = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such form" });
   }
-  
-  const { name, sectors, agree } = req.body;
-  let emptyFields = [];
-  if (!name) {
-    emptyFields.push("name");
-  }
-  if (sectors.length === 0) {
-    emptyFields.push("sectors");
-  }
-  if (!agree) {
-    emptyFields.push("agree");
-  }
   try {
-  const form = await Form.findOneAndUpdate({ _id: id }, { ...req.body });
-  if (!form) {
-    return res.status(400).json({ error: "No such form" });
-  }
-  if (emptyFields.length > 0) {
-    return res.status(400).json({ error: "Please fill in all fields", emptyFields });
-  }
-  else{res.status(200).json(form);}
-  }
-  catch (error) {
+    const existingForm = await Form.findById(id);
+    if (!existingForm) {
+      return res.status(400).json({ error: "No such form" });
+    }
+    const { name, sectors, agree } = req.body;
+    const updatedForm = {
+      name: name || existingForm.name,
+      sectors: sectors || existingForm.sectors,
+      agree: agree || existingForm.agree,
+    };
+    if (!updatedForm.name || updatedForm.sectors.length === 0 || !updatedForm.agree) {
+      return res.status(400).json({ error: "Please fill in all fields" });
+    }
+    const form = await Form.findOneAndUpdate({ _id: id }, updatedForm, { new: true });
+    res.status(200).json(form);
+  } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
