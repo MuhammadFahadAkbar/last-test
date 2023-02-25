545c28d3cacd696497057bca8e4c408a7f62039c
MuhammadFahadAkbar <70443460+MuhammadFahadAkbar@users.noreply.github.com>
2023-02-25 15:11:35 +0500

    Update formController.js

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -52,11 +52,31 @@ const editForm = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such form" });
   }
+    let emptyFields = [];
+  if (!name) {
+    emptyFields.push("name");
+  }
+  if (sectors.length === 0) {
+    emptyFields.push("sectors");
+  }
+  if (!agree) {
+    emptyFields.push("agree");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+  try {
   const form = await Form.findOneAndUpdate({ _id: id }, { ...req.body });
   if (!form) {
     return res.status(400).json({ error: "No such form" });
   }
   res.status(200).json(form);
+  }
+  catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
