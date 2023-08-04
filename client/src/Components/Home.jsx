c6b0987bef5d4bf537ea1fd64d6e5615a3f33af6
Muhammad Fahad Akbar <fahadakbar200@gmail.com>
2023-08-04 22:40:55 +0500

    Update Home.jsx

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="max-w-[95%] mx-auto my-10 md:max-w-screen-xl md:my-20">
-      <h4 className="mb-3 font-semibold text-center text-xl md:text-3xl md:mb-10 lg:text-4xl">
+      <h4 className="mb-3 font-semibold text-center text-xl md:text-3xl md:mb-10 lg:text-4xl text-red-500">
         LET US KNOW THE SECTORS YOU'RE WORKING IN
       </h4>
       <p className="text-justify pb-10 md:w-[90%] md:m-auto md:text-2xl md:font-light lg:w-[81%]">
@@ -20,10 +20,10 @@ const Home = () => {
         professional network and take your skills to the next level.
       </p>
       <div className="flex justify-center">
-        <button className="px-5 py-3 mr-3 bg-black text-white md:py-3 md:px-20 md:text-lg md:mr-5">
+        <button className="px-5 py-3 mr-3 bg-red-950 text-white md:py-3 md:px-20 md:text-lg md:mr-5">
           <Link to={`/form`}>FILL OUT OUR FORM</Link>
         </button>
-        <button className="px-5 py-3 ml-3 bg-black text-white md:py-3 md:px-20 md:text-lg md:ml-5">
+        <button className="px-5 py-3 ml-3 bg-red-950 text-white md:py-3 md:px-20 md:text-lg md:ml-5">
           <Link to={`/list`}>CHECK ENTRIES</Link>
         </button>
       </div>
