e95850aa64eebc180d1c9d41c3200fe5400fefab
Muhammad Fahad Akbar <fahadakbar200@gmail.com>
2023-08-04 22:53:26 +0500

    Update NotFound.jsx

--- client/src/Components/NotFound.jsx
+++ client/src/Components/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="mx-auto my-10 max-w-screen-xl text-center">
-      <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl">404</h1>
-      <p className="text-md sm:text-lg md:text-xl mb-5">PAGE NOT FOUND</p>
+      <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-red-600">404</h1>
+      <p className="text-md sm:text-lg md:text-xl mb-5 text-red-600">PAGE NOT FOUND</p>
 
-      <button className="px-10 py-3 bg-black text-white md:py-3 sm:px-20 sm:text-lg">
+      <button className="px-10 py-3 bg-red-900 text-white md:py-3 sm:px-20 sm:text-lg">
         <Link to={`/`}>HOME</Link>
       </button>
     </div>
