ae4e8752660598768c6be2746083bb6758618770
Muhammad Fahad Akbar <fahadakbar200@gmail.com>
2023-08-04 22:49:57 +0500

    Update EditForm.jsx

--- client/src/Components/EditForm.jsx
+++ client/src/Components/EditForm.jsx
@@ -55,7 +55,7 @@ function EditForm() {
 
   if (!form) {
     return (
-      <div className="text-center text-3xl text-green-600 mt-10">
+      <div className="text-center text-3xl text-red-900 mt-10">
         Loading...
       </div>
     );
@@ -67,7 +67,7 @@ function EditForm() {
         onSubmit={handleSubmit}
         className="p-6 mx-auto shadow-custom border-[1px] md:p-12 md:max-w-screen-sm lg:max-w-screen-md"
       >
-        <h3 className="w-[98%] text-center m-auto font-semibold mb-5 md:text-center md:font-semibold md:text-xl md:mb-10 md:w-[80%] lg:text-2xl">
+        <h3 className="w-[98%] text-center m-auto font-semibold mb-5 md:text-center md:font-semibold md:text-xl md:mb-10 md:w-[80%] lg:text-2xl text-red-900">
           Please edit your name and pick the Sectors you are currently involved
           in.
         </h3>
@@ -79,7 +79,7 @@ function EditForm() {
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
-          className=" w-[70%] ml-7 mb-5 border border-black rounded md:py-3 md:px-4 focus:outline-none focus:bg-white focus:border-green-600 md:ml-14 md:mb-8 md:w-[64%]"
+          className=" w-[70%] ml-7 mb-5 border border-black rounded md:py-3 md:px-4 focus:outline-none focus:bg-white focus:border-red-600 md:ml-14 md:mb-8 md:w-[64%]"
         />
         <br />
         <label htmlFor="sectors" className="text-lg  md:text-xl">
@@ -97,7 +97,7 @@ function EditForm() {
             );
             setSectors(selectedOptions);
           }}
-          className="w-[70%] ml-20 mb-5 border border-black rounded md:py-3 md:px-4 focus:outline-none focus:bg-white focus:border-green-600 md:ml-[7rem] md:mb-8 md:w-[64%]"
+          className="w-[70%] ml-20 mb-5 border border-black rounded md:py-3 md:px-4 focus:outline-none focus:bg-white focus:border-red-600 md:ml-[7rem] md:mb-8 md:w-[64%]"
         >
           {sectorOptions?.map((sector) => (
             <option key={sector.value} value={sector.text}>
@@ -113,7 +113,7 @@ function EditForm() {
             checked={agree}
             onChange={() => setAgree(!agree)}
             required
-            className="w-5 h-5 -2 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            className="w-5 h-5 -2 text-red-600 bg-red-100 border-red-300 rounded focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-red-800 focus:ring-2 dark:bg-red-700 dark:border-red-600"
           />
           <span className="text-md md:text-xl font-light ml-4">
             I agree to the terms and conditions
@@ -123,11 +123,11 @@ function EditForm() {
         <div className="flex justify-center w-[90%] m-auto">
           <button
             type="submit"
-            className="px-5 py-3 mr-3 bg-black text-white md:py-3 md:px-20 md:text-lg md:mr-5"
+            className="px-5 py-3 mr-3 bg-red-900 text-white md:py-3 md:px-20 md:text-lg md:mr-5"
           >
             SUBMIT
           </button>
-          <button className="px-5 py-3 ml-3 bg-black text-white md:py-3 md:px-20 md:text-lg md:ml-5">
+          <button className="px-5 py-3 ml-3 bg-red-900 text-white md:py-3 md:px-20 md:text-lg md:ml-5">
             <Link to={`/list`}>BACK</Link>
           </button>
         </div>
