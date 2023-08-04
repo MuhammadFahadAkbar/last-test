69fd1c1ce86d2fd6582f366d0f192ff6c3638778
Muhammad Fahad Akbar <fahadakbar200@gmail.com>
2023-08-04 22:44:14 +0500

    Update Navbar.jsx

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <div className="bg-gray-100 max-w-screen-xl mx-auto text-center py-3 md:py-4 lg:py-5">
+      <div className="bg-red-100 max-w-screen-xl mx-auto text-center py-3 md:py-4 lg:py-5">
         <Link to="/">
-          <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-700">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-red-900">
             Test Project
           </h1>
         </Link>
